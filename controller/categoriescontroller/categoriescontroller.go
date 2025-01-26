55201124325ba2eab3d4f0b9b57724d50aa5ef77
dreamergirl01 <rahma01twins@gmail.com>
2025-01-26 20:39:47 +0700

    edit, delete process

--- controller/categoriescontroller/categoriescontroller.go
+++ controller/categoriescontroller/categoriescontroller.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"produk_go/entities"
 	"produk_go/models/categorymodel"
+	"strconv"
 	"time"
 )
 
@@ -36,14 +37,71 @@ func Add(w http.ResponseWriter, r *http.Request){
 		category.CreatedAt = time.Now()
 		category.UpdatedAt = time.Now()
 
-		categorymodel.Create(category)
+		if ok := categorymodel.Create(category); !ok{
+			temp, _ := template.ParseFiles("views/category/add.html")
+			temp.Execute(w, nil)
+		}
+
+		http.Redirect(w, r, "/categories", http.StatusSeeOther)
 	}
 }
 
 func Edit(w http.ResponseWriter, r *http.Request){
+	//tampilan edit
+	if r.Method == "GET" {
+		temp, err := template.ParseFiles("views/category/edit.html")
+		if err != nil {
+			panic(err)
+		}
+		//convert string ke integer untuk id
+		idString := r.URL.Query().Get("id")
+		id, err := strconv.Atoi(idString)
 
+		if err != nil {
+			panic(err)
+		}
+
+		//model
+		category := categorymodel.Detail(id)
+		data := map[string]any{
+			"category": category,
+		}
+
+		temp.Execute(w, data)
+	}
+	//proses edit
+	if r.Method == "POST" {
+		var category entities.Category
+
+		idString := r.FormValue("id")
+		id, err := strconv.Atoi(idString)
+
+		if err != nil {
+			panic(err)
+		}
+
+		category.Name = r.FormValue("name")
+		category.UpdatedAt = time.Now()
+
+		if ok := categorymodel.Update(id, category); !ok {
+			http.Redirect(w, r, r.Header.Get("referer"), http.StatusSeeOther)
+			return
+		}
+
+		http.Redirect(w, r, "/categories", http.StatusSeeOther)
+	}
 }
 
 func Delete(w http.ResponseWriter, r *http.Request){
+	idString := r.URL.Query().Get("id")
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		panic(err)
+	}
 
+	if err := categorymodel.Delete(id); err != nil {
+		panic(err)
+	}
+
+	http.Redirect(w, r, "/categories", http.StatusSeeOther)
 }
--- models/categorymodel/categorymodel.go
+++ models/categorymodel/categorymodel.go
@@ -5,6 +5,7 @@ import (
 	"produk_go/entities"
 )
 
+//proses add
 func GetAll() []entities.Category {
 	rows, err := config.DB.Query(`SELECT * FROM categories`)
 	if err != nil {
@@ -17,7 +18,7 @@ func GetAll() []entities.Category {
 
 	for rows.Next() {
 		var category entities.Category
-		if err := rows.Scan(&category.Id, &category.Name, &category.CreatedAt, &category.UpdatedAt); err != err{
+		if err := rows.Scan(&category.Id, &category.Name, &category.CreatedAt, &category.UpdatedAt); err != err {
 			panic(err)
 		}
 
@@ -27,6 +28,54 @@ func GetAll() []entities.Category {
 	return categories
 }
 
-func Create(category entities.Category)  {
-	// config.DB.Exec(``)
+func Create(category entities.Category) bool {
+	result, err := config.DB.Exec(`
+	INSERT INTO categories (name, created_at, updated_at)
+	VALUE (?,?,?)`,
+	category.Name, category.CreatedAt, category.UpdatedAt,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	LastInsertId, err := result.LastInsertId()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return LastInsertId > 0
+}
+
+//proses edit
+func Detail(id int) entities.Category {
+	row := config.DB.QueryRow(`SELECT id, name FROM categories WHERE id = ?`, id)
+
+	var category entities.Category
+	if err := row.Scan(&category.Id, &category.Name); err != nil{
+		panic(err.Error())
+	}
+
+	return category
+}
+
+func Update(id int, category entities.Category) bool {
+	query, err := config.DB.Exec(`UPDATE categories SET name = ?, updated_at = ? WHERE id = ?`, category.Name, category.UpdatedAt, id)
+
+	if err != nil {
+		panic(err)
+	}
+
+	result, err := query.RowsAffected()
+	if err != nil {
+		panic(err)
+	}
+
+	return result > 0
+}
+
+func Delete(id int) error {
+	_, err := config.DB.Exec(`DELETE FROM categories WHERE id = ?`, id)
+	return err
 }
